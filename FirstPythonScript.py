cc38cdfa7f7d87e3c320f80fa6f564608b033c81
egmiller <rellimdet@gmail.com>
2021-01-12 21:03:52 -0600

    This is my first commit

---
+++ FirstPythonScript.py
@@ -0,0 +1,12 @@
+# First Python Script
+# By EG Miller
+
+## Basic Python Things
+
+my_integer = 3
+my_decimal = 3.0
+my_string = '3'
+my_list = [1,2,'apple',19,-2.353,"..."]
+my_dictionary = {'key':'value', 'lookup_key':'find_value'}
+
+print("Hello world!")
